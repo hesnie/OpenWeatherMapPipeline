37856ca6e3841aad0e2cd1cceaaa3e6fd316a18c
Henrik Søby Nielsen <hesnie@gmail.com>
2025-06-14 21:20:57 -0400

    Refactor dbt models and DAG for weather data processing; add new models, remove obsolete files, and enhance data storage integration

---
+++ dags/dbt/open_weather_map/models/dim_date.sql
@@ -0,0 +1,14 @@
+"""
+-- This model creates a date dimension table for the Open Weather Map project.
+-- It uses the dbt_date package to generate a date dimension from 1990-01-01 to 2050-12-31.
+-- The date dimension can be used for time-based analysis in the Open Weather Map project.
+"""
+{{ config(
+    alias='dim_date',
+    materialized='table',
+    schema='open_weather_map'
+) }}
+
+{% set date_dimension = dbt_date.get_date_dimension("1990-01-01", "2050-12-31") %}
+
+{{ date_dimension }}
--- dags/weather_dag.py
+++ dags/weather_dag.py
@@ -6,6 +6,7 @@ This DAG is designed to run daily, fetching the latest weather data and processi
 from airflow.sdk.definitions.asset import Asset
 from airflow.decorators import dag, task
 from airflow.sdk.operators.bash import BashOperator
+from airflow.io.path import ObjectStoragePath
 from pendulum import datetime
 from airflow.sdk import Variable
 #import logging #TODO: setup azure blob storage for log storage
@@ -34,15 +35,16 @@ def weather_data():
         Fetches weather data from the Meteomatics API for a specific location and time range.
         This function queries the API for temperature, UV index, precipitation, and wind speed data at a specified location
         """
-        # Setup vars for API call
-        coordinates = [(47.11, 11.47)]
-        parameters = ['t_2m:C', 'precip_1h:mm', 'wind_speed_10m:ms', 'wind_dir_10m:d', 'uv:idx']
+        import pandas as pd # Used to handle the data returned from the API
+
+        # Setup vars for API call, fetching one days worth of weather data for a specific location, at 1 hour intervals.
+        parameters = ['t_2m:C', 'precip_1h:mm', 'wind_speed_10m:ms', 'wind_dir_10m:d', 'uv:idx', 'weather_code_1h:idx']
         model = 'mix'
         startdate = dt.datetime.now(datetime.timezone.utc).replace(minute=0, second=0, microsecond=0)
         enddate = startdate + dt.timedelta(days=1)
         interval = dt.timedelta(hours=1)
         
-        # Fetch the API key from Airflow variables
+        # Fetch variables from Airflow
         try:
             API_USER = Variable.get('API_USER')
             API_SECRET = Variable.get('API_SECRET')
@@ -50,67 +52,68 @@ def weather_data():
             #logging.error("Failed to fetch params from Airflow")
             raise
 
-        # Query the Meteomatics API for station list in Germany
+        # Query the Meteomatics API for station list in Germany with the dataconnector
         df_stations = api.query_station_list(
-            API_USER, 
-            API_SECRET, 
-            location='germany')
-                
+            username=API_USER, 
+            password=API_SECRET, 
+            location='germany') # Limit to Germany for the free API
 
         # Construct and call the API with the dataconnector
-        df_weather = api.query_time_series(
-            coordinates, 
-            startdate, 
-            enddate, 
-            interval, 
-            parameters, 
-            API_USER, 
-            API_SECRET, 
+        df_weather = api.query_station_timeseries(
+            hash_ids=df_stations['hash_id'].tolist()[0:1],  # Limit to one station for the free API
+            startdate=startdate, 
+            enddate=enddate, 
+            interval=interval, 
+            parameters=parameters, 
+            username=API_USER, 
+            password=API_SECRET, 
             model=model)
+        
+        # Setup the Azure Blob Storage path for saving the data
+        # The path is structured to save station data and weather data in separate directories
+        base = ObjectStoragePath("abfs://blob-meteomatic", conn_id="az_blob_storage")
+        station_path = base / f"stations/station_data_{startdate}-{enddate}.parquet"
+        weather_path = base / f"weather/weather_data_{startdate}-{enddate}.parquet"
 
-        return df_weather
+        # Save the dataframes to Azure Blob Storage in Parquet format
+        with station_path.open("w") as file:
+            df_stations.to_parquet(file)
+            # TODO: Should log the filetransfer to Azure Blob Storage
+
+        with weather_path.open("w") as file:
+            df_weather.to_parquet(file)
+            # TODO: Should log the filetransfer to Azure Blob Storage
+
+        # Push filepaths into xcom for later tasks to use
+        # This allows subsequent tasks in the DAG to access the saved data files
+        get_weather_data.xcom_push(key="station_path", value=station_path)
+        get_weather_data.xcom_push(key="weather_path", value=weather_path)
 
     # Call the task to add it to the DAG
     get_weather_data()
 
     @task(
         # Dataset outlet for the dbt task
-        outlets=["weather_data_raw"]
-    )
-    def save_raw_weather_data(**context):
-        """
-        Save raw weather data to Azure blob storage
-        This function saves the raw weather data fetched from the Meteomatics API to Azure Blob Storage.
-        The data is saved in a CSV format, which can be used for further processing or analysis.
-        """    
-        
-        # Define paths to dbt project and virtual environment
-        PATH_TO_DBT_PROJECT =  "/home/user/dbt_projects/openweathermap_dbt"
-        PATH_TO_DBT_VENV = "/home/user/venvs/dbt_env/bin/activate"
-
-        dbt_run = BashOperator(
-            task_id="dbt_run",
-            bash_command="source $PATH_TO_DBT_VENV && dbt run --models .",
-            env={"PATH_TO_DBT_VENV": PATH_TO_DBT_VENV},
-            cwd=PATH_TO_DBT_PROJECT,
-        )
-
-    # Call the task to add it to the DAG
-    save_raw_weather_data()
-
-    @task(
-        # Dataset outlet for the dbt task
         outlets=["weather_data_transformed"]
     )
-    def transform_and_save_weather_data(**context):
+    def run_dbt_transformations(**context):
         """
-        Transform raw weather data to the needed data structures, and saves them in Azure SQL
-        This function transforms the raw weather data into a star schema format suitable for analytics and reporting.
-        The transformed data is then saved in Azure SQL, making it accessible for further analysis or reporting.
+        Run dbt transformations to process the raw weather data.
+        This function triggers dbt to run the transformations defined in the dbt project,
+        which processes the raw weather data into a star schema format.
         """
+        # Define the dbt command to run the transformations
+        dbt_command = "dbt run --profiles-dir /opt/airflow/dags/dbt/profiles --project-dir /opt/airflow/dags/dbt/open_weather_map"
+
+        # Use BashOperator to execute the dbt command
+        BashOperator(
+            task_id="run_dbt_transformations",
+            bash_command=dbt_command,
+            dag=context['dag'],
+        ).execute(context)
 
     # Call the task to add it to the DAG
-    transform_and_save_weather_data()
+    run_dbt_transformations()
 
 # Instantiate the DAG
 weather_data()
